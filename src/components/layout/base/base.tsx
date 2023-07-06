1a4597071e65c4765741e0e9668ee9d2c7c0db08
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-07-06 19:06:20 +0100

    further project structuring

--- src/components/layout/base/base.tsx
+++ src/components/layout/base/base.tsx
@@ -1,3 +1,16 @@
-export const Base = () => {
-  return <>Base Layout</>;
+import { Header, Navbar } from "..";
+import * as React from "react";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const Base: React.FC<IProps> = ({ children }) => {
+  return (
+    <main>
+      <Header />
+      <Navbar />
+      <main>{children}</main>
+    </main>
+  );
 };
--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,2 +1,3 @@
 export * from "./base"
 export * from "./header"
+export * from "./navbar"
