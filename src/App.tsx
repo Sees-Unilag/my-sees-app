7e7b411f194e1b5a7a226aab5e3d6ad1d083118e
Tobipeters <oluwatobilobapeterschem@gmail.com>
2023-07-17 17:20:25 +0100

    structuring dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 // import logo from "./logo.svg";
 import { Navbar } from "./components";
+import { MainRoutes } from "./routes/main-routes";
 
 function App() {
-  return <div>
-    <Navbar />
-  </div>;
+  return (
+    <>
+      <MainRoutes />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,19 @@
+import styles from "./home.module.css";
+import ITDefense from "../../asset/images/IT-defense.png";
+
+const Home = () => {
+  return (
+    <section className="container">
+      <section className="row">
+        <div className="col-lg-6">
+          <div className={styles.trendingCard}>
+            <img src={ITDefense} className="img-fluid" alt="IT Defense" />
+          </div>
+        </div>
+        <div className="col-lg-6"></div>
+      </section>
+    </section>
+  );
+};
+
+export default Home;
