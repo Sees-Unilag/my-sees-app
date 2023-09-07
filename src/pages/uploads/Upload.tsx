4274bffd28c946b311ae79ca79580de42a6375b7
vhickjr <victormathias001@gmail.com>
2023-09-07 15:13:21 +0100

    minor changes

--- src/pages/uploads/Upload.tsx
+++ src/pages/uploads/Upload.tsx
@@ -4,10 +4,21 @@ import styles from "./upload.module.css";
 
 const Upload = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSemesterDropdownOpen, setIsSemesterDropdownOpen] = useState(false);
+  const [isCoursesDropdownOpen, setIsCoursesDropdownOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
+  const toggleSemesterDropdown = () => {
+    setIsSemesterDropdownOpen(!isSemesterDropdownOpen);
+  };
+
+  const toggleCoursesDropdown = () => {
+    setIsCoursesDropdownOpen(!isCoursesDropdownOpen);
+  };
+
   return (
     <div className={styles.uploadcontainer}>
       <div className={styles.uploadmaterials}>Upload Materials</div>
@@ -22,6 +33,7 @@ const Upload = () => {
             </button>
             {isDropdownOpen && (
               <ul className={styles.dropdownMenu}>
+                {/* Add your level options here */}
                 <li>
                   <a href="/Social">Year 1</a>
                 </li>
@@ -45,13 +57,55 @@ const Upload = () => {
       <div className={styles.upload}>
         <a>First Semester</a>
         <a>
-          <FaChevronDown />
+          <div className={styles.dropdown}>
+            <button
+              onClick={toggleSemesterDropdown}
+              className={styles.dropdownToggle}
+            >
+              <a>
+                <FaChevronDown />
+              </a>
+            </button>
+            {isSemesterDropdownOpen && (
+              <ul className={styles.dropdownMenu}>
+                {/* Add your first semester options here */}
+                <li>
+                  <a href="#">Option 1</a>
+                </li>
+                <li>
+                  <a href="#">Option 2</a>
+                </li>
+                {/* Add more options as needed */}
+              </ul>
+            )}
+          </div>
         </a>
       </div>
       <div className={styles.upload}>
         <a>Select courses</a>
         <a>
-          <FaChevronDown />
+          <div className={styles.dropdown}>
+            <button
+              onClick={toggleCoursesDropdown}
+              className={styles.dropdownToggle}
+            >
+              <a>
+                <FaChevronDown />
+              </a>
+            </button>
+            {isCoursesDropdownOpen && (
+              <ul className={styles.dropdownMenu}>
+                {/* Add your course selection options here */}
+                <li>
+                  <a href="#">Course 1</a>
+                </li>
+                <li>
+                  <a href="#">Course 2</a>
+                </li>
+                {/* Add more course options as needed */}
+              </ul>
+            )}
+          </div>
         </a>
       </div>
       <div className={styles.uploadbox}>Upload material</div>
@@ -67,4 +121,5 @@ const Upload = () => {
     </div>
   );
 };
+
 export default Upload;
